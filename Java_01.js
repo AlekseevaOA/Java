e4e3f49891ab1672c6ab8be209e93da433211ea2
AlekseevaOA <113351171+AlekseevaOA@users.noreply.github.com>
2023-02-14 11:42:18 +0300

    Add files via upload

---
+++ Java_01.js
@@ -0,0 +1,19 @@
+'Привет, Java!'
+var myThing;
+myThing=5;
+myThing="это строка"
+var numberNine=9;
+var stringNine="9";
+numberNine+numberNine;
+stringNine+stringNine;
+var greeting="Привет,";
+var myNime="Ник";
+greeting+myNime;
+myNime.length
+
+var name_gri, name_vich, 
+num_gri_wed, num_gri_fri, num_vich_wen, num_vich_fri, num_gri_week, num_vich_week;
+name_gri="Грипп";
+name_vich="ВИЧ";
+var num_gri_wed=2;
+var num_gri_fri=1;
